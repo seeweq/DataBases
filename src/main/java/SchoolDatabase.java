eca89266c63e218181155b839e1d0f28f9a5a6db
sivu <sivuyisiwet34@gmail.com>
2021-09-13 15:43:03 +0200

    add database browser and generate data

--- src/main/java/SchoolDatabase.java
+++ src/main/java/SchoolDatabase.java
@@ -4,11 +4,11 @@ import com.github.javafaker.Faker;
 import java.sql.*;
 
 public class SchoolDatabase {
-    static int id;
-    static  int unique(int ids){
-        id +=ids;
-        return id;
-    }
+//    static int id;
+//    static  int unique(int ids){
+//        id +=ids;
+//        return id;
+//    }
 
     public static void main(String[] args)
     {
@@ -21,40 +21,42 @@ public class SchoolDatabase {
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
             statement.executeUpdate("drop table if exists personaldetails");
-            statement.executeUpdate("create table personaldetails(id INT PRIMARY KEY, firstName string, lastName String,email String, streetAddress String)");
-            String excuteInsert = "INSERT INTO personaldetails(id,firstName,lastName,email,streetAddress) VALUES (?,?,?,?,?)";
+            statement.executeUpdate("create table personaldetails( firstName string, lastName String,email String, streetAddress String)");
+            String excuteInsert = "INSERT INTO personaldetails(firstName,lastName,email,streetAddress) VALUES (?,?,?,?)";
 
-
-            for (int i=0;i < 100_000;i++) {
+            PreparedStatement preparedStmt = connection.prepareStatement(excuteInsert);
+            for (int i=0;i < 2000;i++) {
                 Faker faker = new Faker();
 
-                int id = unique(1);
+//                int id = unique(1);
                 String firstName = faker.name().firstName();
                 String lastName = faker.name().lastName();
                 String email = faker.internet().emailAddress();
                 String streetAddress = faker.address().streetAddress();
-                PreparedStatement preparedStmt = connection.prepareStatement(excuteInsert);
+
 
                 //set parameters in the statement
-                preparedStmt.setInt(1, id);
-                preparedStmt.setString(2, firstName);
-                preparedStmt.setString(3, lastName);
-                preparedStmt.setString(4, email);
-                preparedStmt.setString(5, streetAddress);
+
+                preparedStmt.setString(1, firstName);
+                preparedStmt.setString(2, lastName);
+                preparedStmt.setString(3, email);
+                preparedStmt.setString(4, streetAddress);
 
                 //execute statement
                 preparedStmt.executeUpdate();
 
 
 
-                ResultSet rs = statement.executeQuery("SELECT * from personaldetails");
-                while (rs.next()) {
-                    // read the result set
 
-                    System.out.println("name = " + rs.getString("firstName"));
-                    System.out.println("email = " + rs.getString("email"));
-                    System.out.println("id = " + rs.getInt("id"));
-                }
+
+            }
+            ResultSet rs = statement.executeQuery("SELECT * from personaldetails");
+            while (rs.next()) {
+                // read the result set
+
+                System.out.println("name = " + rs.getString("firstName"));
+                System.out.println("email = " + rs.getString("email"));
+                System.out.println("id = " + rs.getInt("id"));
             }
 
 
